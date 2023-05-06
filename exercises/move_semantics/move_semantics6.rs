aca87f8b70a429eb84d297d141327f20703e6e63
Wehi <notstrangequark@gmail.com>
2023-05-06 21:13:10 +0200

    progress up to move sematics

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,24 +2,24 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
 
 fn main() {
-    let data = "Rust is great!".to_string();
+    let rust = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(rust.clone());
 
-    string_uppercase(&data);
+    string_uppercase(&rust);
 }
 
 // Should not take ownership
 fn get_char(data: String) -> char {
     data.chars().last().unwrap()
+
 }
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+    let newdata = &data.to_uppercase();
 
-    println!("{}", data);
+    println!("{}", newdata);
 }
